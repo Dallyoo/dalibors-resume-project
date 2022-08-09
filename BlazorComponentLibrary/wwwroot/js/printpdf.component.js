233f59c145d5bc098ccd343d60465c4fba5cf751
Dalibor Hon <hon.dalibor@gmail.com>
2022-08-09 23:10:43 +0200

    Move RCL JS scripts to RCL

---
+++ BlazorComponentLibrary/wwwroot/js/printpdf.component.js
@@ -0,0 +1,21 @@
+﻿/**
+ * File downloader wrapper for JSInterop
+ * @param {any} fileName name of the saved file
+ * @param {any} contentStreamReference memory stream of the downloaded file
+ */
+async function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement("a");
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? "";
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+};
+
+// Close modal box
+function closePrintModal() {
+    $('#printModal').modal('hide');
+};
