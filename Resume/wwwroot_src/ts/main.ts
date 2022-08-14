c2bfb5b696b6837a45e1e62d619f87b126af2f20
Dalibor Hon <hon.dalibor@gmail.com>
2022-08-14 22:57:16 +0200

    New design (1)

--- Resume/wwwroot_src/ts/main.ts
+++ Resume/wwwroot_src/ts/main.ts
@@ -1,11 +1,12 @@
 //
 // Main page script
 //
+///<reference path="../../node_modules/bootstrap-dark-5/dist/js/darkmode.d.ts"/>
 
 // Import other components
 import "./website-animations"
 import "./blazor-modal";
-import { BlazorModalDebug} from "./blazor-modal";
+import { BlazorModalDebug } from "./blazor-modal";
 
 // Import styles
 import "../css/scss/styles.scss";
@@ -45,21 +46,26 @@ class Main {
 
     private onDomContentLoaded(): void {
         window.addEventListener("DOMContentLoaded", event => {
-            // Collapse responsive navbar on click outsime
+
+            //Constants
+            const sidebarCollapser = document.querySelector(".burger-button") as HTMLElement;
+            const sidebarCloser = document.querySelector("a[id=closeSideNav]") as HTMLElement;
+            const sidebar = document.querySelector("div[id=sideNav]") as HTMLElement;
+
+            // Collapse responsive navbar on click outside
             // https://stackoverflow.com/questions/50359249/bootsrap-v4-1-closing-a-collapse-menu-when-clicking-outside-of-div
             $(document).mouseup(function (e) {
-                var container = $("#sideNav");
-                var navbarResponsive = $("#navbarResponsive");
-
-                if (container.is(null) && navbarResponsive.is(null)) {
+                var container = $(".sidebar-wrapper");
+                if (!container && !sidebar) {
                     return;
                 };
-
                 var target = e.toElement;
-
                 // if the target of the click isn't the container nor a descendant of the container
                 if (!container.is(target) && container.has(target).length === 0) {
-                    navbarResponsive.collapse('hide');
+                    if ($(sidebar).hasClass("sidebar-hide")) {
+                        $(sidebar).removeClass("sidebar-hide");
+                    }
+                     
                 };
             });
 
@@ -81,27 +87,38 @@ class Main {
                 };
             };
 
-            // Assign onlick function to dark mode selector
-            const darkModeSelector = document.querySelector("div[id=darkmodeSwitch]");
-            if (darkModeSelector != null) {
-                (<HTMLElement>document.querySelector("#darkmode-button")).onclick = function () {
-                    window["darkmode"].toggleDarkMode();
+            // Assign onclick function to navbar collapser and closer
+            if (sidebarCollapser && sidebar) {
+                sidebarCollapser.onclick = function () {
+                    $(sidebar).toggleClass("sidebar-hide");
                 };
 
-                (<HTMLElement>document.querySelector("#darkmode-button-l-1")).onclick = function () {
-                    window["darkmode"].toggleDarkMode();
-                };
-
-                (<HTMLElement>document.querySelector("#darkmode-button-d-1")).onclick = function () {
-                    window["darkmode"].toggleDarkMode();
-                };
-
-                (<HTMLElement>document.querySelector("#darkmode-forget")).onclick = function () {
-                    window["darkmode"].toggleDarkMode();
+                sidebarCloser.onclick = function () {
+                    $(sidebar).toggleClass("sidebar-hide");
                 };
             };
 
-            // Autohide floting icons
+            // Assign onclick function to dark mode selector
+            const darkModeSelector = document.querySelector("div[id=darkmodeSwitch]");
+            if (darkModeSelector != null) {
+                (<HTMLElement>document.querySelector("#darkmode-button")).onclick = function () {
+                    darkmode.toggleDarkMode();
+                };
+
+                (<HTMLElement>document.querySelector("#darkmode-button-l-1")).onclick = function () {
+                    darkmode.toggleDarkMode();
+                };
+
+                (<HTMLElement>document.querySelector("#darkmode-button-d-1")).onclick = function () {
+                    darkmode.toggleDarkMode();
+                };
+
+                (<HTMLElement>document.querySelector("#darkmode-forget")).onclick = function () {
+                    darkmode.toggleDarkMode();
+                };
+            };
+
+            // Autohide floating icons
             // https://bootstrap-menu.com/detail-autohide.html
             var el_autohide = document.querySelector('.floating-icons');
             if (el_autohide) {
@@ -131,22 +148,18 @@ class Main {
                     offset: 74,
                 });
             }
-            else {
-            };
 
-            // Collapse responsive navbar when toggler is visible
-            const navbarToggler = document.body.querySelector(".navbar-toggler") as HTMLElement;
+            // Collapse responsive navbar when clocked on link
             const responsiveNavItems = [].slice.call(
                 document.querySelectorAll("#navbarResponsive .nav-link:not(#cultureSwitcherMenu)")
             );
             responsiveNavItems.map(function (responsiveNavItem) {
                 responsiveNavItem.addEventListener("click", () => {
-                    if (window.getComputedStyle(navbarToggler).display !== "none") {
-                        navbarToggler.click();
+                    if ($(sidebar).hasClass("sidebar-hide")) {
+                        $(sidebar).removeClass("sidebar-hide");
                     }
                 });
             });
-
         });
     }
 }
